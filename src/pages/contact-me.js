60de122c9e05065a55d00f4b9b3d9c19f2a66a1a
Mike Hansford <mjhansford@gmail.com>
2020-10-24 00:31:17 +1100

    Adjust imports

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
-// import SEO from "../components/seo"
+import SEO from "../components/seo"
 
 export default function ContactMe () {
     return (
         <Layout>
+            <SEO title="Contact Me" />
             <h2>Contact Me</h2>
             <p>You can view my full resume on my <a href="https://www.linkedin.com/in/mikehansford/" target="_blank">LinkedIn Profile.</a></p>
             <p>Or check out my <a href="https://twitter.com/mikehansford10" target="_blank">Twitter account</a>.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
-// import SEO from "../components/seo"
+import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <SEO title="Home" />
     <h2>Hi I'm Mike</h2>
     <p>I'm a software developer with diverse experience across Microsoft SharePoint, Microsoft Office
         and general web development.</p>
@@ -22,9 +23,11 @@ const IndexPage = () => (
     
     <p>I blog about web development, Microsoft Power Platform and Microsoft 365 though I'll likely touch on other topics of interest. </p>
     
-    <p>This site is defintely a work in progress too. It's been rather a while since I designed a website from scratch, 
+    <p>This site is defintely a work in progress too. It's somewhat bare-bones right now. 
+      It's been rather a while since I designed a website from scratch, 
       having worked predominantly on established websites.
-      This site is being built (presently) in Gatsby and hosted on Netlify. I have a lot to learn and it should be a lot of fun.
+      This site is being built (presently) in Gatsby and hosted on Netlify. 
+      I have a lot to learn and it should be a lot of fun.
     </p>
   </Layout>
 )
