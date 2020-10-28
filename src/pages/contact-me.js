21c44025509b3ddb28d6be34a10d6de2a51ba19b
Mike Hansford <mjhansford@gmail.com>
2020-10-28 22:17:12 +1100

    Add rel="noreferrer" to anchor elements

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -9,9 +9,9 @@ export default function ContactMe () {
         <Layout>
             <SEO title="Contact Me" />
             <h2>Contact Me</h2>
-            <p>You can view my full resume on my <a href="https://www.linkedin.com/in/mikehansford/" target="_blank">LinkedIn Profile.</a></p>
-            <p>Or check out my <a href="https://twitter.com/mikehansford10" target="_blank">Twitter account</a>.</p>
-            <p>Yes, I have a <a href="https://github.com/mikehans" target="_blank">Github</a> account but it's not terribly interesting...</p>
+            <p>You can view my full resume on my <a href="https://www.linkedin.com/in/mikehansford/" target="_blank" rel="noreferrer">LinkedIn Profile.</a></p>
+            <p>Or check out my <a href="https://twitter.com/mikehansford10" target="_blank" rel="noreferrer">Twitter account</a>.</p>
+            <p>Yes, I have a <a href="https://github.com/mikehans" target="_blank" rel="noreferrer">Github</a> account but it's not terribly interesting...</p>
         </Layout>
     )
 }
