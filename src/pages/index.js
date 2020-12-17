b872cdfb1df80f510cf248db7668a8d6b15dd3f9
Mike Hansford <mjhansford@gmail.com>
2020-12-18 01:45:26 +1100

    Home page content update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h2>Hi I'm Mike</h2>
-    <p>I'm a software developer with diverse experience across Microsoft SharePoint, Microsoft Office, Microsoft 365
-        and general web development.</p>
+    <p>I'm a software developer with extensive experience in web development, Microsoft SharePoint, Microsoft Office, Microsoft 365.</p>
 
-    <p>I'm busy preparing for the step in my software journey, learning Power Platform and Microsoft 365.</p>
-
+    <p>I blog about web development, Microsoft Power Platform and Microsoft 365 though I'll likely touch on other topics of interest. </p>
+    
     <p>I live in Melbourne, Australia with family. I'm something of a gardener, woodworker and electronics hack.
     I try to grow vegetables and am periodically wonder what
     to do with a glut in some vegetable or another. My last go at radish pickles failed miserably, though I'm sure the
@@ -21,7 +20,6 @@ const IndexPage = () => (
 
     <p> I'm trying to figure out how to make better
         use of my Raspberry Pi and am trying to build a moisture monitoring system for my garden with my Arduino gear.</p>
-    <p>I blog about web development, Microsoft Power Platform and Microsoft 365 though I'll likely touch on other topics of interest. </p>
     
     <p>This site is defintely a work in progress too. It's somewhat bare-bones right now. 
       It's been rather a while since I designed a website from scratch, 
