36a8297ca7f9d8b48333031a0931019c737d162f
Mike Hansford <mjhansford@gmail.com>
2021-01-28 21:45:33 +1100

    Apply layout to JSX pages in lieu of MDX update

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -6,30 +6,47 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import dateFormatter from '../components/utilities/dateFormatter';
 import './blog.css'
+import styles from './reactPage.module.css'
 
-function BlogPage ({data}) {
- 
-    return (
-        <Layout>
-            <SEO title="Blog" />
+function BlogPage({ data }) {
+
+  useEffect(() => {
+    document.body.classList.add('hasBackgroundImage');
+  });
+
+  return (
+    <Layout>
+      <SEO title="Blog" />
+
+      <div className={styles.content}>
+        <section className={styles.contentSection}>
+          <div className={styles.contentSectionInner}>
+
             <h2>Latest Posts</h2>
 
             <div className="cards">
               {data.allMarkdownRemark.edges.map(post => (
-                  <div key={post.node.id} className="card">
-                      <h3 className="blogPostTitle">{post.node.frontmatter.title}</h3>
-                      <div className="blogSummary">
-                        <div className="blogPostDate">{dateFormatter(post.node.frontmatter.date, 'long')}</div>
-                        <div className="blogByline">{post.node.frontmatter.author} </div>
-                        <div className="blogReadMore"><Link to={post.node.frontmatter.path}>Read more...</Link></div>
-                      </div>
+                <div key={post.node.id} className="card">
+                  <h3 className="blogPostTitle">{post.node.frontmatter.title}</h3>
+                  <div className="blogSummary">
+                    <div className="blogPostDate">{dateFormatter(post.node.frontmatter.date, 'long')}</div>
+                    <div className="blogByline">{post.node.frontmatter.author} </div>
+                    <div className="blogReadMore"><Link to={post.node.frontmatter.path}>Read more...</Link></div>
                   </div>
+                </div>
               ))}
             </div>
 
+          </div>
+        </section>
+      </div>
 
-        </Layout>
-    )
+
+
+
+
+    </Layout>
+  )
 }
 
 export const pageQuery = graphql`
